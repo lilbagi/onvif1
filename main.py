af806f0a6a99152f109e602ffa9ead7a5122a1b5
lilbagi <47556276+lilbagi@users.noreply.github.com>
2019-03-04 23:26:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from onvif import ONVIFService
 mycam = ONVIFCamera('192.168.15.42',80,'boshandi7','GtN0aXIBL9is','C:/Python27/wsdl/')
 #mycam = ONVIFCamera('192.168.15.42',80,'boshandi7','GtN0aXIBL9is','C:/Program Files (x86)/Python37-32/Lib/site-packages/wsdl/')
 print (mycam.devicemgmt.GetDeviceInformation())
- 
-"""def zeep_pythonvalue(self, xmlvalue):
-   return xmlvalue
-zeep.xsd.simple.AnySimpleType.pythonvalue = zeep_pythonvalue"""
+
 
 control.instlinfo(mycam)
 control.absolute_move(mycam, -0.2, -0.6, 0)
